c679a02f1a282db541435e8616bb37b24aaae08a
mdaitarek <md.arifulislamtarek6650@gmail.com>
2025-07-31 23:45:05 +0600

    feat(task): get task paginated && update status service done

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { Task, TaskStatus } from 'src/tasks/schemas/task.schema';
-import { CreateTaskDto } from 'src/tasks/dto';
+import { CreateTaskDto, GetTasksDto, UpdateTaskStatusDto } from 'src/tasks/dto';
 
 @Injectable()
 export class TasksService {
@@ -24,4 +24,55 @@ export class TasksService {
     });
   }
 
+  async list(query: GetTasksDto) {
+    const { status, dueFrom, dueTo, search, page = 1, limit = 10 } = query;
+
+    const filter: any = {
+      ...(status && { status }),
+      ...((dueFrom || dueTo) && {
+        dueDate: {
+          ...(dueFrom && { $gte: new Date(dueFrom) }),
+          ...(dueTo && { $lte: new Date(dueTo) }),
+        },
+      }),
+      ...(search && { title: { $regex: search, $options: 'i' } }),
+    };
+
+
+    const skip = (page - 1) * limit;
+
+    const data = await this.taskModel.find(filter)
+      .skip(skip)
+      .limit(limit)
+      .exec();
+
+
+    const total = await this.taskModel.countDocuments(filter);
+
+    const totalPages = Math.ceil(total / limit);
+
+    return {
+      data,
+      meta: {
+        page,
+        limit,
+        total,
+        totalPages,
+      },
+    };
+  }
+
+  async updateStatus(id: string, updateDto: UpdateTaskStatusDto) {
+    const task = await this.taskModel.findByIdAndUpdate(
+      id,
+      { status: updateDto.status },
+      { new: true },
+    );
+
+    if (!task) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+
+    return task;
+  }
 }
